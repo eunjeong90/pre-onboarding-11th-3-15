f61973204b9dbbd37bf6cd14e71eaecfe4a257c1
JangEunJeong <cuttlefish77777@gmail.com>
2023-07-12 00:11:59 +0900

    feat: Header, MainPage, DetailPage 컴포넌트 추가

---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <header style={{ width: '100%', backgroundColor: 'pink', height: '50px' }}>header</header>
+    </>
+  );
+}
