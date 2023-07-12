e012df066049c9609388adb96ad9b3c5c79c18db
JangEunJeong <cuttlefish77777@gmail.com>
2023-07-13 02:00:36 +0900

    feat: 메인 페이지 구현 및 무한스크롤 구현

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useClient } from 'Context/clientContext';
+import { IIssueChild } from 'Interface/gitHubAPI';
+import ListItem from 'Components/ListItem';
+import useOnScreen from 'hooks/useOnScreen';
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  const { isLoading, hasMore, issueList, handleLoadList } = useClient();
+  const { measureRef, isIntersecting, observer } = useOnScreen();
+
+  useEffect(() => {
+    if (isIntersecting && hasMore) {
+      handleLoadList();
+      observer?.disconnect();
+    }
+  }, [isIntersecting, hasMore, handleLoadList]);
+
+  return (
+    <ul>
+      {issueList.map((issues: IIssueChild, index: number) => {
+        if (index === issueList.length - 1) {
+          return <ListItem key={issues.id} issues={issues} measureRef={measureRef} />;
+        }
+        return <ListItem key={issues.id} issues={issues} />;
+      })}
+      {isLoading && <div style={{ height: '50px' }}>isLoading...</div>}
+    </ul>
+  );
 }
---
+++ src/hooks/useOnScreen.tsx
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react';
+
+const useOnScreen = ({ root = null, rootMargin = '0px', threshold = 1.0 } = {}) => {
+  const [observer, setOserver] = useState<IntersectionObserver>();
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const measureRef = useCallback(
+    (node: Element) => {
+      if (node) {
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            setIntersecting(entry.isIntersecting);
+          },
+          { root, rootMargin, threshold },
+        );
+
+        observer.observe(node);
+        setOserver(observer);
+      }
+    },
+    [root, rootMargin, threshold],
+  );
+
+  return { measureRef, isIntersecting, observer };
+};
+
+export default useOnScreen;
