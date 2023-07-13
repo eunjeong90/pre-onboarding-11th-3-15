62051754ea429a516f1b29e6eb086f63e68f1512
JangEunJeong <cuttlefish77777@gmail.com>
2023-07-13 11:49:07 +0900

    fix: 디테일 페이지 마크다운 영역 적용

--- src/Pages/DetailPage.tsx
+++ src/Pages/DetailPage.tsx
@@ -2,14 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useClient } from 'Context/clientContext';
 import { IIssueChild } from 'Interface/gitHubAPI';
+import MarkdownRenderer from 'Components/Common/MarkdownRenderer/MarkdownRenderer';
 
 export default function DetailPage() {
   const params = useParams();
   const { issueId } = params;
   const { getIssueDetailPage } = useClient();
   const [issueDetail, setIssueDetail] = useState<IIssueChild>({} as IIssueChild);
+
   useEffect(() => {
     getIssueDetailPage(Number(issueId)).then(setIssueDetail);
   }, [getIssueDetailPage, issueId]);
-  return <div>{issueDetail.id}</div>;
+
+  return (
+    <>
+      <p>번호 {issueDetail.number}</p>
+      <p>제목 {issueDetail.title}</p>
+      <p>작성자 {issueDetail.user?.login}</p>
+      <p>작성일 {issueDetail.created_at}</p>
+      <p>코멘트 수 {issueDetail.comments}</p>
+      <p>작성자 프로필 이미지 {issueDetail.user?.avatar_url}</p>
+      <MarkdownRenderer>{issueDetail.body}</MarkdownRenderer>
+    </>
+  );
 }
